fb8061cdd80d8ad0b61657cd45639470908ff635
Jean-Michel Marin <jean-michel.marin@umontpellier.fr>
2016-01-23 00:55:18 +0000

    version 1.1

--- R/abcrf.R
+++ R/abcrf.R
@@ -1,5 +1,4 @@
-abcrf.factor <- function(modindex, sumsta, lda=TRUE, ntree=500, 
-                    sampsize=min(1e5, length(modindex)), ...) 
+abcrf.factor <- function(modindex, sumsta, lda=TRUE, ntree=500, sampsize=min(1e5, length(modindex)), paral=FALSE, ...) 
 {
 	if (!is.factor(modindex)) 
     stop("modindex should be a factor containing the model indexes")
@@ -10,7 +9,7 @@ abcrf.factor <- function(modindex, sumsta, lda=TRUE, ntree=500,
     stop("no simulation in the reference table (modindex, sumstat)")
   if ( (!is.logical(lda)) && (length(lda) != 1L))
     stop("lda should be TRUE or FALSE")
-  if (is.null(colnames(sumsta))) colnames(sumsta)= paste("V",1:dim(sumsta)[2],sep="")
+  if (is.null(colnames(sumsta))) colnames(sumsta) <- paste("V",1:dim(sumsta)[2],sep="")
   if (lda) {
   	model.lda <- lda(sumsta, modindex)
     sumsta <- cbind(sumsta, predict(model.lda)$x)
@@ -20,11 +19,31 @@ abcrf.factor <- function(modindex, sumsta, lda=TRUE, ntree=500,
   m <- names(match.call(expand.dots=TRUE))
   if ((!"sampsize" %in% m) && (nrow(sumsta) <= 15)) 
     sampsize <- as.integer(sampsize / 10)
-	model.rf <- randomForest(sumsta, modindex, ntree=ntree, sampsize=sampsize, ...)
+  if (paral==TRUE) {
+    ncores <- max(detectCores()-1,1) 
+    cl <- makeCluster(ncores)
+    registerDoParallel(cl)
+    if (trunc(ntree/ncores)==ntree/ncores) ntrees <- rep(ntree/ncores, ncores) else
+      ntrees <- c(rep(trunc(ntree/ncores), ncores),ntree-trunc(ntree/ncores)*ncores)
+    model.rf <- foreach(ntree=ntrees, .combine= combine, .multicombine=TRUE, .packages='randomForest') %dopar% {
+        randomForest(sumsta, modindex, ntree=ntree, sampsize=sampsize, norm.votes=FALSE, keep.inbag=TRUE, ...)
+      }
+    stopCluster(cl)
+    tbl <- table(model.rf$predicted, model.rf$y)
+    class.error = vector()
+
+    for (i in 1:nrow(tbl)) {
+      rowSum <- sum(tbl[i,])
+      accurate <- diag(tbl)[i]
+      error <- rowSum - accurate
+      class.error[i] <- error / rowSum
+    }   
+    model.rf$confusion <- cbind(tbl, class.error)
+    colnames(model.rf$confusion) <- c(paste(1:nlevels(modindex)),"class.error")
+  } else model.rf <- randomForest(sumsta, modindex, ntree=ntree, sampsize=sampsize, norm.votes=FALSE, ...)
   cl <- match.call()
   cl[[1]] <- as.name("abcrf")
-  x <- list(call=cl, lda=lda, model.lda=model.lda, model.rf=model.rf,
-            prior.err=model.rf$err[ntree,])
+  x <- list(call=cl, lda=lda, model.rf=model.rf, model.lda=model.lda, prior.err=mean(model.rf$predicted!=model.rf$y), sumsta=sumsta)
   class(x) <- "abcrf"
   x
 }
@@ -61,12 +80,9 @@ abcrf <- function(...) UseMethod("abcrf")
 
 print.abcrf <- function(x, ...) {
   cat("\nCall:\n", deparse(x$call), "\n")
-  if (x$lda) 
-    cat("includes the axes of a preliminary LDA\n\n")  
-  
+  if (x$lda) cat("includes the axes of a preliminary LDA\n\n")  
   cat("Number of simulations: ", length(x$model.rf$y), "\n", sep="")
-  cat("     Prior error rate: ", round(x$model.rf$err.rate[x$model.rf$ntree, 
-      "OOB"] * 100, digits = 4), "%\n\n", sep = "")
+  cat("     Prior error rate: ", round(x$prior.err * 100, digits = 4), "%\n\n", sep = "")
   cat("Confusion matrix:\n")
   print(x$model.rf$confusion, ...)  
 }
--- R/predict.abcrf.R
+++ R/predict.abcrf.R
@@ -1,24 +1,35 @@
-predict.abcrf <- function(object, sumsta, obs, ...)
+predict.abcrf <- function(object, obs, ntree=1000, sampsize=min(1e5, dim(sumsta)[1]), paral=FALSE, ...)
 {
-	if (is.vector(obs)) obs=matrix(obs,1,length(obs))
-	if (is.null(colnames(sumsta))) {
-	  colnames(sumsta)= paste("V",1:dim(sumsta)[2],sep="")
-	  colnames(obs)=colnames(sumsta)
-	} 
+	modindex <- object$model.rf$y
+  nmod <- nlevels(modindex)
+  sumsta <- object$sumsta
+	if (object$lda) nstat <- ncol(sumsta)-(nmod-1) else nstat <- ncol(sumsta)
+  if (is.vector(obs)) obs <- matrix(obs,1,length(obs))
 	if (is.null(colnames(obs))) {
-	  colnames(obs)=colnames(sumsta)
+	  colnames(obs) <- colnames(sumsta)[1:nstat]
     warning("Columns of obs have no names")
 	}
 	old.options <- options(); options(warn=-1)
 	if (object$lda) {
-		obs <- cbind(obs, predict(object$model.lda ,obs)$x)
-		sumsta <- cbind(sumsta, predict(object$model.lda ,sumsta)$x)
+	  model.lda <- lda(sumsta[,1:nstat],modindex)
+		obs <- cbind(obs, predict(model.lda,obs)$x)
 	}
+	vote <- predict(object$model.rf, obs, type="vote", norm.votes=FALSE)
 	allocation <- predict(object$model.rf, obs)
-	local.error <- as.numeric(object$model.rf$predicted==object$model.rf$y)
-	error.rf <- randomForest(sumsta, local.error, ...)
+	local.error <- as.numeric(object$model.rf$predicted==modindex)
+	  if (paral==TRUE) {
+    ncores <- max(detectCores()-1,1) 
+    cl <- makeCluster(ncores)
+    registerDoParallel(cl)
+    if (trunc(ntree/ncores)==ntree/ncores) ntrees <- rep(ntree/ncores, ncores) else
+      ntrees <- c(rep(trunc(ntree/ncores), ncores),ntree-trunc(ntree/ncores)*ncores)
+    error.rf <- foreach(ntree=ntrees, .combine= combine, .multicombine=TRUE, .packages='randomForest') %dopar% {
+        randomForest(sumsta, local.error, ntree=ntree, sampsize=sampsize, ...)
+      }
+    stopCluster(cl)
+  } else error.rf <- randomForest(sumsta, local.error, ntree=ntree, sampsize=sampsize, ...)
 	options(old.options)
-	tmp <- list(allocation=allocation,post.prob=predict(error.rf, obs))
+	tmp <- list(allocation=allocation, vote=vote, post.prob=predict(error.rf, obs))
   class(tmp) <- "abcrfpredict"
   tmp
 }
@@ -29,23 +40,23 @@ summary.abcrfpredict <- function(object, ...) {
 }
 
 print.abcrfpredict <- function(x, ...) {
-  ret <- cbind(x$allocation, x$post.prob)
-  colnames(ret) <- c("model", "post.proba")
+  ret <- cbind(x$allocation, x$vote, x$post.prob)
+  colnames(ret) <- c("selected model", paste("votes model",1:dim(x$vote)[2],sep=""), "post.proba")
   print(ret, ...)
 }
 
 as.matrix.abcrfpredict <- function(x, ...) {
-  ret <- cbind(x$allocation, x$post.prob)
+  ret <- cbind(x$allocation, x$vote, x$post.prob)
   colnames(ret) <- c("model", "post.proba")
   ret
 }
 
 as.data.frame.abcrfpredict <- function(x, ...) {
-  ret <- cbind(x$allocation, x$post.prob)
+  ret <- cbind(x$allocation, x$vote, x$post.prob)
   colnames(ret) <- c("model", "post.proba")
   as.data.frame(ret,  row.names=NULL, optional=FALSE, ...)
 }
 
 as.list.abcrfpredict <- function(x, ...) {
-  list(allocation = x$allocation,  post.prob = x$post.prob, ...)
+  list(allocation = x$allocation, vote = x$vote, post.prob = x$post.prob, ...)
 }
